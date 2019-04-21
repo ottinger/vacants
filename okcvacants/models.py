b40cc7ab225e0a5ffc5a08c9facce76e016fa61e
Michael Ottinger <michael@ottinger.net>
2019-04-20 23:13:15 -0400

    Initial commit: Create Django project. Create Property model in okcvacants/models.py. Write initial version of parser for vacants pdf file in okcvacants/management/import_pdf.py.

---
+++ okcvacants/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Property(models.Model):
+    # This will contain the fields in the Abandoned Buildings List for now.
+    # May break the data down/add more fields later.
+    case_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=150)
+    declared_date = models.DateTimeField()
+    ward_number = models.IntegerField()
+    parcel_number = models.IntegerField()  # This is OKC's parcel number, not to be confused with County Assessor's!
