a6e94929df7e9721df3dde97d252c041139167b6
Michael Ottinger <michael@ottinger.net>
2019-04-27 16:09:48 -0400

    Scrap react for now

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -1,30 +1,12 @@
-class Map extends React.Component {
-    componentDidMount() {
-        var mymapp = L.map('mapp').setView([35.4676, -97.5164], 13);
-        L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
-            maxZoom: 18,
-        }).addTo(mymapp);
+var mymapp = L.map('mappy').setView([35.4676, -97.5164], 13);
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+    maxZoom: 18,
+}).addTo(mymapp);
 
-        var mygeojson = getGeoJson();
-        console.log(mygeojson);
-        L.geoJSON(mygeojson).addTo(mymapp);
+var mygeojson = getGeoJson();
+console.log(mygeojson);
+L.geoJSON(mygeojson).addTo(mymapp);
 
-        var neighborhoodsGeoJson = getNeighborhoodsGeoJson();
-        L.geoJSON(neighborhoodsGeoJson).addTo(mymapp);
-    }
-
-    render() {
-        return <div id="mapp"></div>
-    }
-}
-
-ReactDOM.render(<Map/>, document.getElementById('mappy'));
-
-class Address extends React.Component {
-    render() {
-        return <h1>Hello World</h1>
-    }
-}
-
-ReactDOM.render(<Address/>, document.getElementById('curaddress'));
+var neighborhoodsGeoJson = getNeighborhoodsGeoJson();
+L.geoJSON(neighborhoodsGeoJson).addTo(mymapp);
