84f9c6edb50dc139471e476c59f27471d65f2eef
Michael Ottinger <michael@ottinger.net>
2019-05-02 02:43:55 -0400

    change it back

--- vacants_project/settings.py
+++ vacants_project/settings.py
@@ -137,7 +137,6 @@ STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
