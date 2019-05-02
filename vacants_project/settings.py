5c1157961196e54a2c739930b9475c1f6f26543d
Michael Ottinger <michael@ottinger.net>
2019-05-02 03:38:51 -0400

    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

--- vacants_project/settings.py
+++ vacants_project/settings.py
@@ -142,6 +142,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # serializers
 SERIALIZATION_MODULES = {
     "geojson": "django.contrib.gis.serializers.geojson"
