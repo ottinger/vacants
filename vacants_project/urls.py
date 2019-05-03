29e3467fbaffd67f1120326a4b081270ec45ff12
Michael Ottinger <michael@ottinger.net>
2019-05-03 01:12:50 -0400

    Add redirect from root to okcvacants/

--- vacants_project/urls.py
+++ vacants_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('okcvacants/', include('okcvacants.urls'))
+    path('okcvacants/', include('okcvacants.urls')),
+    path('', lambda r: HttpResponseRedirect('okcvacants/'))
 ]
