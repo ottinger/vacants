10049ac846127594e3226a36e8af6b1799f4dcb1
Michael Ottinger <michael@ottinger.net>
2019-05-04 04:54:37 -0400

    Convert property markers in Leaflet from Marker to CircleMarker (faster)

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -4,21 +4,6 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voya
     maxZoom: 18,
 }).addTo(mymapp);
 
-// Get geojson for properties
-var mygeojson = getGeoJson();
-L.geoJSON(mygeojson, {
-    onEachFeature: function (feature, layer) {
-        if (feature.properties && feature.properties.address) {
-            var popStr = "";
-            popStr += feature.properties.address;
-
-            if (feature.properties.pk)
-                popStr += "<br><a href=\"" + propertyRoot + feature.properties.pk + "\">More...</a>";
-            layer.bindPopup(popStr);
-        }
-    }
-}).addTo(mymapp);
-
 // Get geojson for neighborhoods
 var neighborhoodsGeoJson = getNeighborhoodsGeoJson();
 L.geoJSON(neighborhoodsGeoJson, {
@@ -32,5 +17,26 @@ L.geoJSON(neighborhoodsGeoJson, {
             layer.bindPopup(popStr);
 
         }
+    }
+}).addTo(mymapp);
+
+// Get geojson for properties
+var mygeojson = getGeoJson();
+L.geoJSON(mygeojson, {
+    onEachFeature: function (feature, layer) {
+        if (feature.properties && feature.properties.address) {
+            var popStr = "";
+            popStr += feature.properties.address;
+
+            if (feature.properties.pk)
+                popStr += "<br><a href=\"" + propertyRoot + feature.properties.pk + "\">More...</a>";
+            layer.bindPopup(popStr);
+        }
+    },
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            radius: 10,
+            color: "#F00"
+        })
     }
 }).addTo(mymapp);
